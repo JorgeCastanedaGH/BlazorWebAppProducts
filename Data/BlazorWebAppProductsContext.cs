52833c6046e0dd6e06d5defe4e0213709006e170
castacorp <124871317+castacorp@users.noreply.github.com>
2025-08-09 14:30:54 -0600

    Initial

---
+++ Data/BlazorWebAppProductsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppProducts.Models;
+
+namespace BlazorWebAppProducts.Data
+{
+    public class BlazorWebAppProductsContext : DbContext
+    {
+        public BlazorWebAppProductsContext (DbContextOptions<BlazorWebAppProductsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppProducts.Models.Product> Product { get; set; } = default!;
+    }
+}
